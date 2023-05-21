97dacef15c65d530830e5ad69af7a33427fc7785
Borja Orge Gesteira <borja_1089@hotmail.com>
2023-05-21 21:44:26 +0800

    data

---
+++ Utes/Utes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Utes
+{
+    public partial class Utes : Form
+    {
+        public Utes()
+        {
+            InitializeComponent();
+        }
+    }
+}
